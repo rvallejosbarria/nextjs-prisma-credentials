063fbb5267d4bf020dc4d0b8ec7cae6fff99f803
Roberto Vallejos Barria <rvallejos@puertoangamos.cl>
2024-06-13 15:01:38 -0400

    register page

---
+++ app/api/auth/register/route.js
@@ -0,0 +1,44 @@
+import { NextResponse } from 'next/server'
+
+import bcrypt from 'bcrypt'
+import db from '@/libs/db'
+
+export async function POST(request) {
+  try {
+    const data = await request.json()
+
+    const userFound = await db.user.findUnique({
+      where: {
+        email: data.email
+      }
+    })
+
+    if (userFound) {
+      return NextResponse.json({
+        message: "Email already exists"
+      }, {
+        status: 400
+      })
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10)
+
+    const newUser = await db.user.create({
+      data: {
+        username: data.username,
+        email: data.email,
+        password: hashedPassword
+      }
+    })
+
+    const {password: _, ...user} = newUser
+
+    return NextResponse.json(user)
+  } catch (error) {
+    return NextResponse.json({
+      message: error.message
+    }, {
+      status: 500
+    })
+  }
+}
---
+++ prisma/migrations/20240613173749_init/migration.sql
@@ -0,0 +1,29 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- CreateTable
+CREATE TABLE [dbo].[User] (
+    [id] INT NOT NULL IDENTITY(1,1),
+    [email] NVARCHAR(1000) NOT NULL,
+    [username] NVARCHAR(1000) NOT NULL,
+    [password] NVARCHAR(1000) NOT NULL,
+    [createdAt] DATETIME2 NOT NULL CONSTRAINT [User_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
+    [updatedAt] DATETIME2 NOT NULL,
+    CONSTRAINT [User_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [User_email_key] UNIQUE NONCLUSTERED ([email]),
+    CONSTRAINT [User_username_key] UNIQUE NONCLUSTERED ([username])
+);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
